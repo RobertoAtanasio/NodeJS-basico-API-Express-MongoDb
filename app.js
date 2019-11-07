58fe361d73c7657affd8f66ed998031c6530dd62
Roberto Atanásio <roberto.atanasio.pl@gmail.com>
2019-11-07 15:23:59 -0300

    corrigindo app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
+const app = require('express')();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const config = require('./config/config');
